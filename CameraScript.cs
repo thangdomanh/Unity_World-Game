7e9f9f58a71cf201f06e6466ff7f75aa218376f7
thangdomanh <103508865+thangdomanh@users.noreply.github.com>
2023-05-18 19:48:53 +0700

    Add files via upload

---
+++ CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject grabbedObject;
+
+    public Transform grabPos;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Input.GetKeyDown(KeyCode.E) && Physics.Raycast(transform.position, transform.forward, out hit, 5) && hit.transform.GetComponent<Rigidbody>() && hit.transform.tag == "Grabbable")
+        {
+            Debug.Log("grab");
+            grabbedObject = hit.transform.gameObject;
+        }
+        else if(Input.GetKeyDown(KeyCode.Q))
+        {
+            grabbedObject = null;
+        }
+        if(grabbedObject != null)
+        {
+            grabbedObject.GetComponent<Rigidbody>().velocity = 10 * (grabPos.position - grabbedObject.transform.position);
+        }
+
+    }
+}
---
+++ doorcard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorcard : MonoBehaviour
+{
+    public Animator doorAnim;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grabbable"))
+        {
+            doorAnim.ResetTrigger("Close01");
+            doorAnim.SetTrigger("Open01");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Grabbable"))
+        {
+            doorAnim.ResetTrigger("Open01");
+            doorAnim.SetTrigger("Close01");
+        }
+    }
+}
